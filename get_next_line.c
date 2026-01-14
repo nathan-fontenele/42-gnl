b594dcc73c9d6169f141ee9acd758502ef274ccc
nathan-fontenele <nathangomes.st@gmail.com>
2026-01-14 14:27:24 -0300

    add process_line

--- get_next_line.c
+++ get_next_line.c
@@ -12,6 +12,28 @@
 
 #include "get_next_line.h"
 
+char *process_line(char **stash)
+{
+    char *line;
+    char *leftovers;
+    int len;
+    
+    len = 0;
+    while ((*stash[len] != '\n') && (*stash[len] != '\0'))
+        len++;
+    if ((*stash[len] == '\n'))
+    {
+        line = ft_substr(*stash, 0, len + 1);
+        leftovers = ft_substr(*stash, len + 1, ft_strlen(*stash) (len + 1));
+        free(*stash);
+        *stash = leftovers;
+        return (line);
+    }
+    return (NULL);
+}
+
+
+
 char *get_next_line(int fd)
 {
     
